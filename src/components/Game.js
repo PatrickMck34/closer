8e653e2e0c93b0c15b9d4ca8eb935bbfa1e0613e
PatrickMck34 <corkscrewhollow@gmail.com>
2023-11-23 16:01:41 -0500

    start screen

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,72 +1,98 @@
 import React, { Component } from 'react';
+import { useEffect } from 'react';
 const room1 = `url("https://wallpapercave.com/wp/wp7517920.jpg")`
 const room2 = `url("https://i.etsystatic.com/42685745/r/il/f7c78c/4908711238/il_fullxfull.4908711238_ifxx.jpg")`
 const room3 = `url("https://cdn1.epicgames.com/ue/product/Screenshot/Screenshot23-1920x1080-faad1d637e8cf81a9fcbc32994ce875b.jpg?resize=1&w=1920")`
 let selector = room1
-const map = [4,4]
+let room = 0
+let key = 0
+if(room === 0)selector= room1
+if(room === 1) selector = room2
+if(room === 2)selector = room3
+
+function KeyUp() {
+    key = 1
+    console.log("Key Up")
+}
+
 function  handleRoomChange (event) {
-     if(selector = room1){
-         selector = room2
+    switch (selector){
+        case room1: {
+            alert("You enter the room")
+            selector = room2
+            break;
+        }
+        case room2: {
+            selector = room3
+            break;
+        }
+        case room3: {
+            selector = room1
+            break;
+        }
+    }
         
- 
- }  if(selector = room2){
-     selector = room3
+}
 
- } 
- 
- else { selector = room3}
- }
- function  handleRoomChange2 () {
-//     if(selector = room3){
-//         selector = room2
-
-// }  if(selector = room2){
-//     selector = room1
-
-} 
-// if(map === ([4,4])){
-   
-// }
-//  }
-class Game extends Component {
-    
-    state = {
-        x: 160,
-        y: 350,
-       
-        interactiveAreas: [
-            { x: 100, y: 100, width: 100, height:400,  action: () => handleRoomChange()},
-            { x: 950, y: 100, width: 100, height:400,  action: () => handleRoomChange2() }
-        ],
-    };
-    handleKeyDown = (event) => {
-        const { x, y, interactiveAreas } = this.state;
-        switch (event.key) {
-            case 'ArrowUp':
-                this.setState({ y: y - 15 });
+    function  handleRoomChange2 () {
+        switch (selector){
+            case room1: {
+              Game.x = 1000
                 break;
-            case 'ArrowDown':
-                this.setState({ y: y + 15 });
+            }
+            case room2: {
+                selector = room1
                 break;
-            case 'ArrowLeft':
-                this.setState({ x: x - 15 });
+            }
+            case room3: {
+                selector = room2
                 break;
-            case 'ArrowRight':
-                this.setState({ x: x + 15 });
+            }
+        }
+            } 
+          
+            
+                class Game extends Component {
+                    
+                    state = {
+                        x: 600,
+                        y: 350,
+                        
+                        interactiveAreas: [
+                            { x: 100, y: 100, width: 100, height:400,  action: () => handleRoomChange()},
+                            { x: 950, y: 100, width: 100, height:400,  action: () => handleRoomChange2() }
+                        ],
+                        
+                    };
+                    
+                    handleKeyDown = (event) => {
+                        const { x, y, interactiveAreas } = this.state;
+                        switch (event.key) {
+                            case 'ArrowUp':
+                                this.setState({ y: y - 15 });
+                                break;
+                                case 'ArrowDown':
+                                    this.setState({ y: y + 15 });
+                                    break;
+                                    case 'a':
+                                        this.setState({ x: x - 15 });
+                                        break;
+                                        case 'd':
+                                            this.setState({ x: x + 15 });
                 break;
             default:
                 break;
         }
          for (let area of interactiveAreas) {
-      if (x <= 155 ) {
-        area.action();
+      if (x <= 155 && key >0 ) {
+        handleRoomChange();
         this.setState({ x: (this.state.x + 800), y: (this.state.y) });
       }
-    //   if (x >= 1000  ) {
-    //     area.action();
-    //     this.setState({ x: (this.state.x - 800), y: (this.state.y),  });
+      if (x >= 1000  ) {
+        handleRoomChange2();
+        this.setState({ x: (this.state.x - 800), y: (this.state.y),  });
        
-    //   }
+      }
     
 
     }
@@ -85,9 +111,32 @@ class Game extends Component {
 
     render() {
         const { x, y } = this.state;
+        
+       
+ 
         return (
             <div style={{ backgroundImage: selector ,position: 'relative', height: '100vh', width: '100vw', backgroundSize: 'cover' }}>
                 <div style={{ position: 'absolute', top: y, left: x }}><img src="https://lh3.googleusercontent.com/a/ACg8ocKw1_fT-luZQq61YLYr9o7tjRVsONoRRHdOU--V6hqVCQ=s288-c-no" className="h-24 rounded-xl p-1" /></div>
+            <div className="text-white  text-2xl  p-4 flex mx-auto w-96">
+                <div className="flex bg-slate-800/90 rounded-xl p-1 border-2 border-slate-900 shadow-lg shadow-slate-900 mt-24">
+                {(key < 1) ? (
+                    <div>
+                <div className="h-24 flex justify-center text-center items-center">
+
+            Hello, Welcome to my Castle... Shall we begin?
+                </div>
+            <div className="flex items-center justify-around">
+
+                <button className="bg-green-800 border-2 w-24 font-semibold border-green-900 rounded-xl p-1 shadow-lg shadow-green-900" onClick={()=>KeyUp()}>Yes</button>
+                <button className="bg-green-800 border-2 w-24 font-semibold border-green-900 rounded-xl p-1 shadow-lg shadow-green-900" >No</button>
+            </div>
+                        </div>
+                ) : ( <div>
+                    </div>
+                )}
+            </div>
+                </div>
+            <div className="flex mt-[18em] text-red-600 ">LVL 1</div>
             </div>
         );
     }
